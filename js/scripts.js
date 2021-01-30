537ac8ed6b49fa7dafea49d58011f12c545ab102
Lucas de Lima da Silva <lucaasls@hotmail.com>
2021-01-30 10:48:02 -0300

    subindo o projeto

---
+++ js/scripts.js
@@ -0,0 +1,148 @@
+const Modal = {
+    open() {
+        //Abrir Modal
+        //Adicionar active no Modal
+        document
+            .querySelector('.modal-overlay')
+            .classList.add('active');
+    },
+    close() {
+        //Fechar Modal
+        //Retirar Active do Modal
+        document
+            .querySelector('.modal-overlay')
+            .classList.remove('active');
+
+    }
+}
+
+const Transaction = {
+    all: [
+        {
+            description: 'Luz',
+            amount: -50000,
+            date: '20/01/2021'
+        },
+        {
+            description: 'Site',
+            amount: 500000,
+            date: '20/01/2021'
+        },
+        {
+            description: 'Net',
+            amount: -200000,
+            date: '20/01/2021'
+        }],
+    add(transaction) {
+        Transaction.all.push(transaction)
+        App.reload()
+    },
+
+
+    remove(index) {
+        Transaction.all.splice(index, 1)
+        App.reload()
+    },
+
+    incomes() {
+        let income = 0;
+        Transaction.all.forEach((transaction) => {
+            if (transaction.amount > 0) {
+                income += transaction.amount;
+            }
+        })
+        return income;
+    },
+    expenses() {
+        expense = 0;
+        Transaction.all.forEach((transaction) => {
+            if (transaction.amount < 0) {
+                expense += transaction.amount;
+            }
+        })
+        return expense;
+    },
+    total() {
+        return Transaction.incomes() + Transaction.expenses();
+    }
+}
+
+//pegar as transações do meu objeto js
+//e colocar no HTML para mostrar na View
+
+const DOM = {
+    transactionContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        DOM.transactionContainer.appendChild(tr)
+    },
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td class="img"><img src="assets/minus.svg" alt="Remover Transação"></td>
+        `
+        return html;
+    },
+
+    updateBalance() {
+        document.getElementById('incomeDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.incomes())
+
+        document.getElementById('expenseDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.expenses())
+
+        document.getElementById('totalDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.total())
+    },
+
+    clearTransactions() {
+        DOM.transactionContainer.innerHTML = ""
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+        value = Number(value) / 100
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value;
+
+    }
+}
+
+const Form = {
+    submit(event) {
+        event.preventDefault()
+    }
+}
+
+const App = {
+    init() {
+
+        Transaction.all.forEach(function (transaction) {
+            DOM.addTransaction(transaction)
+        })
+
+        DOM.updateBalance()
+    },
+    reload() {
+        DOM.clearTransactions()
+        App.init()
+    }
+}
+
+App.init()
+
